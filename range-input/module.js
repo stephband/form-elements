ca64eb436b6122bc34850d30e6dc1a52bcf0c739
Stephen <stephband@cruncher.ch>
2022-06-13 07:36:46 +0200

    Range

--- range-input/module.js
+++ range-input/module.js
@@ -1,17 +1,21 @@
 /**
 <range-input>
 
-Import `<range-input>` custom element. This also registers the custom
-element and upgrades instances already in the DOM.
+Import the `<range-input>` custom element. This registers the custom element and
+upgrades instances already in the DOM:
 
 ```html
 <script type="module" src="range-input.js"></script>
-<range-input name="ratio" min="-1" max="1" ticks="-1 -0.8 -0.6 -0.4 -0.2 0 0.2 0.4 0.6 0.8 1" value="0">Scale</range-input>
-<range-input name="gain" min="-48dB" max="0dB" ticks="-∞dB -48dB -42dB -36dB -30dB -24dB -18dB -12dB -6dB 0dB" scale="log-60dB" display="dB" value="-6dB">Volume</range-input>
-<range-input name="frequency" min="20" max="20000" ticks=max="20000" ticks="20Hz 30Hz 50Hz 100Hz 200Hz 300Hz 500Hz 1kHz 2kHz 3kHz 5kHz 10kHz 20kHz" scale="log" display="Hz" value="20Hz">Frequency</range-input>
-<range-input name="rating" min="1" max="5" ticks="1 2 3 4 5" step="tick" scale="linear" value="3">Rating</range-input>
 ```
 
+You can now write `<range-input>` elements in your HTML:
+
+```html
+<range-input name="ratio" min="-1" max="1" ticks="-1 -0.8 -0.6 -0.4 -0.2 0 0.2 0.4 0.6 0.8 1" value="0">Scale</range-input>
+<range-input name="gain" min="-48dB" max="0dB" ticks="-∞dB -48dB -42dB -36dB -30dB -24dB -18dB -12dB -6dB 0dB" scale="log-60dB" display="dB" value="-6dB">Volume</range-input>
+<range-input name="frequency" min="20" max="20000" ticks="20Hz 50Hz 100Hz 200Hz 500Hz 1kHz 2kHz 5kHz 10kHz 20kHz" scale="log" display="Hz" value="20Hz">Frequency</range-input>
+<range-input name="rating" min="1" max="5" ticks="1 2 3 4 5" step="tick" scale="linear" value="3">Rating</range-input>
+```
 **/
 
 import element    from '../../dom/modules/element.js';
