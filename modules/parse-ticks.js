00224ae6456ae87f246dbbf3c73e29c892722fe0
Stephen <stephband@cruncher.ch>
2023-03-16 17:07:11 +0100

    parseTicks accepts labels with spaces in brackets

--- modules/parse-ticks.js
+++ modules/parse-ticks.js
@@ -1,8 +1,12 @@
 
+import capture    from '../../fn/modules/capture.js';
 import id         from '../../fn/modules/id.js';
+import last       from '../../fn/modules/last.js';
 import overload   from '../../fn/modules/overload.js';
 import toType     from '../../fn/modules/to-type.js';
 import parseValue from './parse-value.js';
+
+const rfloat = /^[+\-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?/;
 
 /**
 parseTicks(string)
@@ -15,35 +19,43 @@ an array of objects of the form:
     value: // The converted numeric value
 }
 ```
+
+Values may optionally be followed by labels in square brackets:
+
+```js
+parseTicks('10ms [Short time]')    // { value: 0.01, label: 'Short time' }
+```
+
+(Square brackets may be omitted where the label contains no numbers or spaces.)
 **/
 
-function toTicks(ticks, value) {
-    const tick = ticks[ticks.length - 1];
-
-    // Does value begin with something other than a number? Must be an
-    // override label.
-    if (tick && !/^-?\d/.test(value)) {
-        tick.label = value;
-    }
-    else {
-        ticks.push({
-            label: value,
-            value: parseValue(value)
-        });
-    }
-
-    return ticks;
-}
-
-export default overload(toType, {
-    string: function parseTicks(string) {
-        const trimmed = string.trim();
-        return trimmed ?
-            trimmed
-            .split(/\s*,\s*|\s+/)
-            .reduce(toTicks, []) :
-            [] ;
+const parseTicks = capture(/^([+\-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?\w*)\s*(?:\[([^\]]+)\]|(\S+))?\s*/, {
+    // value and unit
+    1: (ticks, captures) => {
+        ticks.push({ value: parseValue(captures[1]) });
+        return ticks;
     },
 
+    // [label], in brackets may contain spaces
+    2: (ticks, captures) => {
+        last(ticks).label = captures[2];
+        return ticks;
+    },
+
+    // label, no brackets, single word only
+    3: (ticks, captures) => {
+        last(ticks).label = captures[3];
+        return ticks;
+    },
+
+    // Continue parsing
+    done: (ticks, captures) => parseTicks(ticks, captures),
+
+    // No more ticks
+    catch: (ticks, captures) => ticks
+});
+
+export default overload(toType, {
+    string: (string) => parseTicks([], string.trim()),
     object: id
 });
